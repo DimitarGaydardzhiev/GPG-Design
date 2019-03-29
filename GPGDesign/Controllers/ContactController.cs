98f7552c2df8a0ef4d380ec109dd1f5a71970488
Teofil Yankulov <tyankulov@virtual-affairs.com>
2019-03-29 17:17:51 +0200

    adding contact us page, send method (not finished), refactoring

---
+++ GPGDesign/Controllers/ContactController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using GPGDesign.Models.ContactModels;
+
+namespace GPGDesign.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]        
+        public ActionResult SentEmail(EmailFormModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
+                var message = new MailMessage();
+                message.To.Add(new MailAddress("teofil.y@gmail.com"));  // replace with valid value 
+                message.From = new MailAddress(model.FromEmail);
+                message.Subject = "Your email subject";
+                message.Body = string.Format(body, model.FromName, model.FromEmail, model.Message);
+                message.IsBodyHtml = true;
+
+                using (var smtp = new SmtpClient())
+                {
+                    var credential = new NetworkCredential
+                    {
+                        UserName = "user@outlook.com",  // replace with valid value
+                        Password = "password"  // replace with valid value
+                    };
+                    smtp.Credentials = credential;
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.Port = 587;
+                    smtp.EnableSsl = true;
+                    smtp.SendMailAsync(message);
+                    return RedirectToAction("SentEmail");
+                }
+            }
+            return View(model);
+        }
+
+        public ActionResult SentEmail()
+        {
+            return View();
+        }
+    }
+}
---
+++ GPGDesign/Models/Contact/EmailFormModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GPGDesign.Models.ContactModels
+{
+    public class EmailFormModel
+    {
+        [Required]
+        public string FromName { get; set; }
+
+        [Required]
+        public string FromEmail { get; set; }
+
+        [Required]
+        public string Phone { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+    }
+}
