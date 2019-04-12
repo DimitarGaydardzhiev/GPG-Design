e373ea15756e1a3acc92c21c3214b45c30a06d63
Teofil Yankulov <tyankulov@virtual-affairs.com>
2019-04-12 10:04:24 +0300

    expand category with description

---
+++ DbEntities/Migrations/20190412070147_ExpandCategory.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace DbEntities.Migrations
+{
+    public partial class ExpandCategory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BgDescription",
+                table: "Category",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DeDescription",
+                table: "Category",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "EnDescription",
+                table: "Category",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BgDescription",
+                table: "Category");
+
+            migrationBuilder.DropColumn(
+                name: "DeDescription",
+                table: "Category");
+
+            migrationBuilder.DropColumn(
+                name: "EnDescription",
+                table: "Category");
+        }
+    }
+}
